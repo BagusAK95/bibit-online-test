743981529d02f8cbf9ca84fb69a5aeec7418bac4
BagusAK95 <bagus@kata.ai>
2021-08-11 18:22:37 +0700

    init server

---
+++ omdb/src/main.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import * as http from 'http';
+import * as bodyparser from 'body-parser';
+import cors from 'cors'
+import { RoutesConfig } from './common/config/routes.config';
+import { MovieRoutes } from './movie/movie.routes';
+import debug from 'debug';
+
+const app: express.Application = express();
+const server: http.Server = http.createServer(app);
+const port = 3000;
+const routes: Array<RoutesConfig> = [];
+const debugLog: debug.IDebugger = debug('log');
+
+app.use(bodyparser.json());
+app.use(cors());
+
+routes.push(new MovieRoutes(app));
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.status(200).send(`Server running at http://localhost:${port}`)
+});
+
+server.listen(port, () => {
+    debugLog(`Server running at http://localhost:${port}`);
+    routes.forEach((route: RoutesConfig) => {
+        debugLog(`Routes configured for ${route.getName()}`);
+    });
+});
