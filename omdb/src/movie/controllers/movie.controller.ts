8d800d947d0d1386e63bc338e564b48231c2b2cc
BagusAK95 <bagus@kata.ai>
2021-08-11 19:22:36 +0700

    fix movie controller

--- omdb/src/movie/controllers/movie.controller.ts
+++ omdb/src/movie/controllers/movie.controller.ts
@@ -1,21 +1,21 @@
 import express from 'express';
-import { MovieService } from '../services/movie.service';
+import MovieService from '../services/movie.service';
 
-export class MovieController {
-    constructor(private readonly movie: MovieService) {}
-
+class MovieController {
     async search(req: express.Request, res: express.Response) {
         const title = req.query.title;
-        const page = req.query.page || 1;
+        const page = req.query.page;
 
-        const result = await this.movie.search(title as string, page as number)
+        const result = await MovieService.search(title, page)
         res.status(200).send(result);
     }
 
     async detail(req: express.Request, res: express.Response) {
         const id = req.params.id;
 
-        const result = await this.movie.detail(id)
+        const result = await MovieService.detail(id)
         res.status(200).send(result);
     }
 }
+
+export default new MovieController();
