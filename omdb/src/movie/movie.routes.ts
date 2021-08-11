ded1551f0ad4a5e85e6a5cd7bbed9ce92a175359
BagusAK95 <bagus@kata.ai>
2021-08-11 19:22:26 +0700

    fix movie routes

--- omdb/src/movie/movie.routes.ts
+++ omdb/src/movie/movie.routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { RoutesConfig } from '../common/configs/routes.config';
+import MovieController from '../movie/controllers/movie.controller'
 
 export class MovieRoutes extends RoutesConfig {
     constructor(app: express.Application) {
@@ -7,6 +8,9 @@ export class MovieRoutes extends RoutesConfig {
     }
 
     routes() {
+        this.app.route(`/movie`).get(MovieController.search)
+        this.app.route(`/movie/:id`).get(MovieController.detail)
+
         return this.app;
     }
 }
