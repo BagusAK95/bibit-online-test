78ab998547a8d4a455cb296497f0574a2db67825
BagusAK95 <bagus@kata.ai>
2021-08-11 20:22:26 +0700

    create connection on main

--- omdb/src/main.ts
+++ omdb/src/main.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import * as http from 'http';
-import * as bodyparser from 'body-parser';
-import cors from 'cors'
+import { createConnection } from 'typeorm';
 import { RoutesConfig } from './common/configs/routes.config';
 import { MovieRoutes } from './movie/movie.routes';
+import dbConn from './common/configs/database.config'
 import debug from 'debug';
+import cors from 'cors'
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
@@ -12,7 +13,7 @@ const port = 3000;
 const routes: Array<RoutesConfig> = [];
 const debugLog: debug.IDebugger = debug('log');
 
-app.use(bodyparser.json());
+app.use(express.json());
 app.use(cors());
 
 routes.push(new MovieRoutes(app));
@@ -21,9 +22,14 @@ app.get('/', (req: express.Request, res: express.Response) => {
     res.status(200).send(`Server running at http://localhost:${port}`)
 });
 
-server.listen(port, () => {
-    debugLog(`Server running at http://localhost:${port}`);
-    routes.forEach((route: RoutesConfig) => {
-        debugLog(`Routes configured for ${route.getName()}`);
+createConnection(dbConn).then(() => {
+    server.listen(port, () => {
+        debugLog(`Server running at http://localhost:${port}`);
+        routes.forEach((route: RoutesConfig) => {
+            debugLog(`Routes configured for ${route.getName()}`);
+        });
     });
-});
+}).catch((err) => {
+    debugLog(`Unable to connect to database`);
+    process.exit(1)
+})
